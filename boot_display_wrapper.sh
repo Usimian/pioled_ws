e17ab6b456cda67f18f497356b02463ba8c33e4d
Marc Wester <marc.wester@gmail.com>
2026-06-03 08:11:36 -0400

    Humle to Jazzy fixes

--- boot_display_wrapper.sh
+++ boot_display_wrapper.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 # Wrapper: source ROS2 + workspace so robot_msgs is importable, then run the display.
 set -e
-source /opt/ros/humble/setup.bash
-# Source the workspace that provides robot_msgs. library_ws contains the custom msgs.
+source /opt/ros/jazzy/setup.bash
+# Source the workspace that provides robot_msgs.
 if [ -f /home/mw/yahboomcar_ros2/yahboomcar_ws/install/setup.bash ]; then
     source /home/mw/yahboomcar_ros2/yahboomcar_ws/install/setup.bash
 fi
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
+export ROS_DOMAIN_ID=0
 export CYCLONEDDS_URI=/home/mw/cyclonedds_unicast.xml
 exec /usr/bin/python3 /home/mw/pioled_ws/boot_display.py
